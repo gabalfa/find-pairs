c96c6c229d3dea41a7b2a4f3a68a4dc6881d6852
Gabriel Alfonso <gabrielalfonso@Gabriels-MacBook-Pro.local>
2023-08-14 10:22:16 -0500

    added another function test

--- performance.test.js
+++ performance.test.js
@@ -2,8 +2,11 @@ const findPairs = require("./performance");
 
 describe("findPairs", () => {
   it("should return an array with pairs founded into a list of numbers", () => {
+    /// input the list of numbers
     const listNumbers = [1, 9, 5, 0, 20, -4, 12, 16, 7];
+    /// given value target
     const givenValue = 12;
+    /// result
     const returnedPairs = findPairs(listNumbers, givenValue);
 
     returnedPairs.forEach((pair) => {
@@ -13,8 +16,11 @@ describe("findPairs", () => {
   });
 
   it("should return an array with a specific length", () => {
+    /// input the list of numbers
     const listNumbers = [1, 9, 5, 0, 20, -4, 12, 16, 7];
+    /// given value target
     const givenValue = 12;
+    /// result
     const returnedPairs = findPairs(listNumbers, givenValue);
 
     expect(3).toEqual(returnedPairs.length);
